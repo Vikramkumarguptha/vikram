0254ed1b5868eabc54ee7a8db0bb04994b32379f
Vikramkumarguptha <vikramkumarguptha@gmail.com>
2017-09-14 06:41:13 -1000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package epamproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s1=new Student(101,"vikram",1996-07-15,88);
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
